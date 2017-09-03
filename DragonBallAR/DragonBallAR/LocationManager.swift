3ed11b8c0fc21dd749b48cf1dcd36f5fa55da071
Demetrio <dem3perez@gmail.com>
2017-09-03 11:33:54 -0400

    Adds Location Manager and setup folders

---
+++ DragonBallAR/DragonBallAR/LocationManager.swift
@@ -0,0 +1,13 @@
+//
+//  LocationManager.swift
+//  DragonBallAR
+//
+//  Created by User on 9/3/17.
+//  Copyright © 2017 DemetrioPerez. All rights reserved.
+//
+
+import UIKit
+
+class LocationManager: NSObject {
+
+}
