cbf6b13f0dfe63b49b9640259450c8f0487ebaa6
Demetrio <dem3perez@gmail.com>
2017-09-03 11:10:35 -0400

    Set up transparent grid
    Set up grid on top of map

---
+++ DragonBallAR/DragonBallAR/DragonRadarViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DragonRadarViewController.swift
+//  DragonBallAR
+//
+//  Created by User on 9/3/17.
+//  Copyright © 2017 DemetrioPerez. All rights reserved.
+//
+
+import UIKit
+import MapKit
+class DragonRadarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBOutlet weak var radar: MKMapView!
+    
+
+}
