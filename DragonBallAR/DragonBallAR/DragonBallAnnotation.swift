1de75e4a785c39cc160dc15e1b5c389b9a0c4c72
Demetrio <dem3perez@gmail.com>
2017-09-06 12:11:35 -0400

    Add annotation class

---
+++ DragonBallAR/DragonBallAR/DragonBallAnnotation.swift
@@ -0,0 +1,25 @@
+//
+//  DragonBallAnnotation.swift
+//  DragonBallAR
+//
+//  Created by User on 9/6/17.
+//  Copyright © 2017 DemetrioPerez. All rights reserved.
+//
+
+import UIKit
+import MapKit
+class DragonBallAnnotation: NSObject,MKAnnotation {
+    
+    var coordinate: CLLocationCoordinate2D
+    var title: String?
+    var subtitle: String?
+    init(_ coordinate: CLLocationCoordinate2D, title: String, subtitle: String)
+    {
+        self.coordinate = coordinate
+        self.title = title
+        self.subtitle = subtitle
+    }
+    
+    
+    
+}
--- DragonBallAR/DragonBallAR/DragonRadarViewController.swift
+++ DragonBallAR/DragonBallAR/DragonRadarViewController.swift
@@ -65,6 +65,26 @@ extension DragonRadarViewController:MKMapViewDelegate
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
         return tileRenderer!
     }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if let annotation = annotation as? DragonBallAnnotation {
+            let identifier = "pin"
+            var view: MKPinAnnotationView
+            if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+                as? MKPinAnnotationView { // 2
+                dequeuedView.annotation = annotation
+                view = dequeuedView
+            } else {
+                // 3
+                view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                view.canShowCallout = true
+                view.calloutOffset = CGPoint(x: -5, y: 5)
+          
+            }
+            return view
+        }
+        return nil
+    }
 }
 
 extension DragonRadarViewController:CLLocationManagerDelegate
